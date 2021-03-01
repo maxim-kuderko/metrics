c68e08de31fbb57bc08103875d5e1775bbfab370
Max Kuderko <kuderko@gmail.com>
2021-03-01 15:12:40 +0200

    + add timestamp to add metric

--- drivers/stdout.go
+++ drivers/stdout.go
@@ -1,15 +1,18 @@
 package drivers
 
 import (
-	"fmt"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/maxim-kuderko/metrics/entities"
+	"os"
 )
 
 type Stdout struct {
 }
 
 func (s Stdout) Send(metrics entities.Metrics) {
-	fmt.Println(metrics)
+	for _, m := range metrics {
+		jsoniter.ConfigFastest.NewEncoder(os.Stdout).Encode(m)
+	}
 }
 
 func NewStdout() *Stdout {
