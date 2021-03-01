e7df9a61e040b8c2ed758b31bc008485b64ea596
Max Kuderko <kuderko@gmail.com>
2021-03-01 23:31:46 +0200

    + edit example

--- example/main.go
+++ example/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/maxim-kuderko/metrics"
 	"github.com/maxim-kuderko/metrics/drivers"
+	"math/rand"
 	"time"
 )
 
 func main() {
 	reporter := metrics.NewReporter(metrics.WithDriver(drivers.NewHTTP(`http://localhost:8080/send`, time.Second*10)), metrics.WithConcurrency(1))
-	reporter.Send(`aa`, 1)
+	str := RandStringRunes(20)
+	for {
+		reporter.Send(fmt.Sprintf(`%s%d`, str, rand.Int()%1000000), 1)
+	}
 	reporter.Close()
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
