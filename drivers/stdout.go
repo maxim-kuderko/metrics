d35d086e145c89e0f5f1550c375d15d0002d9c97
Max Kuderko <kuderko@gmail.com>
2021-03-02 14:04:33 +0200

    + make stdout driver use the io.writer driver

--- drivers/stdout.go
+++ drivers/stdout.go
@@ -1,20 +1,9 @@
 package drivers
 
 import (
-	jsoniter "github.com/json-iterator/go"
-	"github.com/maxim-kuderko/metrics/entities"
 	"os"
 )
 
-type Stdout struct {
-}
-
-func (s Stdout) Send(metrics entities.Metrics) {
-	for _, m := range metrics {
-		jsoniter.ConfigFastest.NewEncoder(os.Stdout).Encode(m)
-	}
-}
-
-func NewStdout() *Stdout {
-	return &Stdout{}
+func NewStdout() *Writer {
+	return NewWriter(os.Stdout)
 }
